6108e0342db7db0c613702110f6968e8de8402f7
Christian Aistleitner <christian@quelltextlich.at>
2020-07-27 23:02:49 +0200

    filter: icinga: Add more OSes to image mapping

--- filter_plugins/icinga.py
+++ filter_plugins/icinga.py
@@ -1,10 +1,16 @@
 def map_os_to_image(os):
     os = os.lower()
-    ret = 'unknown.git'
+    ret = 'unknown.gif'
     if os == 'debian':
         ret = 'base/debian.gif'
     elif os == 'gentoo':
         ret = 'vendors/gentoo.gif'
+    elif os == 'hp':
+        ret = 'vendors/hp-ux2.gif'
+    elif os == 'openwrt':
+        ret = 'vendors/linux40.gif'
+    elif os == 'linux':
+        ret = 'vendors/linux40.gif'
     return ret
 
 
